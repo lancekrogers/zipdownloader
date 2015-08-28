a70c91b64ede768be31cc5869983ee4aa0f52ef2
Lance Rogers <lancekincaid1994@gmail.com>
2015-08-28 17:05:43 -0400

    initial commit

---
+++ stealer.py
@@ -0,0 +1,21 @@
+# this is a zip downloader for udacity
+# intro to java course
+# this will not be maintened
+import subprocess as sub
+
+
+
+def download_zips(lessons):
+    counter = 1
+    try:
+        while counter <= lessons:
+            url = "https://s3.amazonaws.com/udacity-hosted-downloads/lesson{}.zip".format(counter)
+            outfile = "zip{}".format(counter)
+            sub.call(["curl", url, "-o", outfile])
+            print(".....curling..zip....file....{}.......".format(counter))
+            counter += 1
+        print('..........Java Lessons Downloaded.......')
+    except:
+        print("......ERROR......Try...Again....")
+
+download_zips(4)
